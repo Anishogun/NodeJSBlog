525ed36ca83bcb5573fbdfb776ee4969e45173c6
Anishogun <anhduy123000@gmail.com>
2021-02-26 18:12:17 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+'use strict';
+var path = require('path');
+var express = require("express");
+var exphbs  = require('express-handlebars');
+var morgan = require("morgan");
+var sass = require('node-sass');
+
+const app = express();
+var hbs = exphbs.create({extname: ".hbs"});
+
+// Set static folders for accessing 
+app.use(express.static(path.join(__dirname,'public')));
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+// Setting port for server
+const port = process.env.PORT || 3000;
+
+app.use(morgan('combined'));
+
+app.get('/',(req, res)=>{
+    res.render('home');
+});
+
+app.listen(port, ()=>{console.log( `Example app listening at http://localhost:${port}`)});
+console.log("sudo kill $(sudo lsof -t -i:3000 to close the port, killall -9 node");
