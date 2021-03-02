03ca4bcf9c96a26fe1fa742e96c77b19fd29583f
Anishogun <anhduy123000@gmail.com>
2021-03-02 15:01:33 +0700

    Husky test8

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-///////////////////////     MODULES     /////////////////////////////
+ ///////////////////////     MODULES     /////////////////////////////
 ///////////////////////   REQUIREMENTS  /////////////////////////////
 
 'use strict';
 var path = require('path');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var morgan = require('morgan');
+var express = require("express");
+var exphbs  = require('express-handlebars');
+var morgan = require("morgan");
 var sass = require('node-sass');
 var fs = require('fs');
 
 const route = require('./routes');
 
-///////////////////////     SETTINGS   //////////////////////////////
+
+ ///////////////////////     SETTINGS   //////////////////////////////
 /////////////////////////////////////////////////////////////////////
 
 const app = express();
-var hbs = exphbs.create({ extname: '.hbs' });
+var hbs = exphbs.create({extname: ".hbs"});
 
-// Set static folders for accessing
-app.use(express.static(path.join(__dirname, 'public')));
+// Set static folders for accessing 
+app.use(express.static(path.join(__dirname,'public')));
 
 // For getting  POST request values in request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded( {extended: true }));
 app.use(express.json());
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'))
 
 // Use morgan for logging.
 app.use(morgan('combined'));
@@ -34,13 +35,12 @@ app.use(morgan('combined'));
 // Setting port for server
 const port = process.env.PORT || 3300;
 
+
 ///////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
 // Routes init
 route(app);
 
-            app.listen(port, () => {
-                      console.log(`Example app listening at http://localhost:${port}`);
-            });
-            console.log('to close the port, killall -9 node');
+app.listen(port, ()=>{console.log( `Example app listening at http://localhost:${port}`)});
+console.log("to close the port, killall -9 node");
